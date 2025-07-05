0db09cebf526fee43e9003f9c0e8f99b8b788585
ItsLasa <manojlasantha306@gmail.com>
2025-07-05 14:55:14 +0530

    app add

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
+// src/App.tsx
+import { Routes, Route } from 'react-router-dom'
+import Admin from './pages/Admin'
+import Home from './pages/Home'
 
-import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
-
-function App() {
-
-
+export default function App() {
   return (
-    <>
-      <div>
-       <Header/>
-        <Footer/>
-      </div>
-        
-    </>
+   
+
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+   
   )
 }
-
-export default App
