ef6a9051560ddc31136f40a9baf04054abbc17f0
ItsLasa <manojlasantha306@gmail.com>
2025-07-07 11:21:53 +0530

    router admin pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@
 import { Routes, Route } from 'react-router-dom'
 import Admin from './pages/Admin'
 import Home from './pages/Home'
+import Add from './pages/Admin/add-product'
+import Products from './pages/Admin/products'
+import Orders from './pages/Admin/orders'
 
 export default function App() {
   return (
@@ -11,6 +14,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/add" element={<Add />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/orders" element={<Orders />} />
         </Routes>
    
   )
